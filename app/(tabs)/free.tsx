a8f1a3393789b01866f7b4a5650709326aae40e3
fatihsenguun <fatihsenguun@gmail.com>
2026-04-03 15:36:59 +0300

    first commit

---
+++ app/(tabs)/free.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const free = () => {
+  return (
+    <View>
+      <Text>asdasdasdasdasdasd</Text>
+    </View>
+  )
+}
+
+export default free
+
+const styles = StyleSheet.create({})
